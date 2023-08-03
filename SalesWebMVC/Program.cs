9e0cd7caa3c86f423d61388dea200cdda1ec1d3b
Gabriel Franco <francobarreto1708@gmail.com>
2023-08-03 12:55:36 -0300

    SellerService, Controller index, injeção de dependencia

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
 
@@ -10,11 +11,13 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<SellerService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
