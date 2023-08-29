36c6a89a2363938b1d0bcfab73b83a32f3b15d61
Gabriel Franco <francobarreto1708@gmail.com>
2023-08-29 16:21:54 -0300

    Correção FK Department e migration

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SalesWebMVCContext _context;
 
         private readonly SellerService _sellerService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, SalesWebMVCContext context)
         {
+            _context = context;
             _sellerService = sellerService;
         }
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            seller.Department = _context.Department.First();
+            //if (!ModelState.IsValid) return View();
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
